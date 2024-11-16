307e2edb33a3680db31dbe10a678d0552ce888d6
WDRoa <willieroa01@gmail.com>
2024-11-16 03:10:54 -0400

    Vulnerabilities have been removed from npm packages and a welcome section has been created on the API home page.

--- config/config.js
+++ config/config.js
@@ -2,19 +2,17 @@ require("dotenv").config();
 
 const config = {
   env: process.env.NODE_ENV || "dev",
+	isProd: process.env.NODE_ENV === "production",
   port: process.env.PORT || 3000,
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbHost: process.env.DB_HOST,
-  dbName: process.env.DB_NAME,
-  dbPort: process.env.DB_PORT,
 	apiKey: process.env.API_KEY,
 	jwtSecret: process.env.JWT_SECRET,
 	jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
 	smtpEmail: process.env.SMTP_EMAIL,
 	smtpPassword: process.env.SMTP_PASSWORD,
 	defaultUserEmail: process.env.DEFAULT_USER_EMAIL,
-	defaultUserPassword: process.env.DEFAULT_USER_PASSWORD
+	defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
+	dbUrl: process.env.DATABASE_URL
 }
 
 module.exports = { config };
+
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { checkApiKey } = require("../middlewares/auth.handler");
+// const { checkApiKey } = require("../middlewares/auth.handler");
 
 const usersRouter = require("./users.router");
 const productsRouter = require("./products.router");
@@ -12,7 +12,40 @@ const routerApi = app => {
 	const router = express.Router();
 	app.use("/api/v1", router);
 
-	router.get("/", checkApiKey, (request, response) => response.send("Welcome to the WJK backend! 🙂"));
+	router.get("/", (request, response) => response.send(`
+		<div>
+			<h1 align="center">🙂 Welcome to the WJK backend! 🙂</h1>
+
+			<h3 align="center">At the moment, the GET endpoints /products and /categories are completely public and<br>
+												can be viewed directly from the browser. The other endpoints and functionalities<br>
+												can be tested through postman or insomnia.</h3>
+
+			<p align="center">Currently, the administrative user's email is <strong>default@user.com</strong> and their
+				password is <strong>Passw@rd1</strong>. This user can use all the API endpoints, which are:<br>
+
+					<h3 align="center">/auth/login</h3>
+					<p align="center">(So that every user can log in)</p>
+					<h3 align="center">/auth/refresh-token</h3>
+					<p align="center">(So that every user doesn't have to log in before 15 days)</p>
+					<h3 align="center">/auth/recovery</h3>
+					<p align="center">(So that every user can start a password reset process)</p>
+					<h3 align="center">/auth/change-password</h3>
+					<p align="center">(So that every user can reset their password)</p>
+					<h3 align="center">/products</h3>
+					<p align="center">(To view and manage all products)</p>
+					<h3 align="center">/categories</h3>
+					<p align="center">(To view and manage all categories)</p>
+					<h3 align="center">/users</h3>
+					<p align="center">(To view and manage all users)</p>
+					<h3 align="center">/orders</h3>
+					<p align="center">(To view and manage all orders)</p>
+					<h3 align="center">/profile/my-orders</h3>
+					<p align="center">(So that every user can view all their orders)</p>
+
+					<h3 align="center">The information required to create, update and delete entity instances will be provided by data validation performed by the middlewares.</h3>
+					<h2 align="center">😊 I wish you a good understanding of the API and have a excellent day! 😊</h2>
+			</p>
+		</div>`));
 
 	router.use("/users", usersRouter);
 	router.use("/products", productsRouter);
